0bce285fe232b11f1768d897197daf5f5bd59032
MatiasCovarrubias <matias.covarrubias@nyu.edu>
2024-05-15 16:59:37 +0200

    corrected Rbc

--- DEQN/econ_models/rbc_ces.py
+++ DEQN/econ_models/rbc_ces.py
@@ -34,7 +34,7 @@ class RbcCES_SteadyState():
         Ydef = (self.alpha**(1/self.sigma_y) * K**((self.sigma_y-1)/self.sigma_y) + (1-self.alpha)**(1/self.sigma_y) * L**((self.sigma_y-1)/self.sigma_y) ) ** (self.sigma_y/(self.sigma_y-1))
 
         C_loss = P/MgUtC - 1
-        L_loss = MgUtC * theta*L**(self.eps_l ** (-1)) / MPL -1
+        L_loss = theta*L**(self.eps_l ** (-1)) / MPL -1
         K_loss = 1/MPK - 1
         I_loss = Pk/P - 1
         P_loss = Y/(C+I) - 1
@@ -291,7 +291,7 @@ class RbcCES_GHH():
         Ydef = A*(self.alpha**(1/self.sigma_y) * K**((self.sigma_y-1)/self.sigma_y) + (1-self.alpha)**(1/self.sigma_y) * L**((self.sigma_y-1)/self.sigma_y) ) ** (self.sigma_y/(self.sigma_y-1))
 
         C_loss = P/MgUtC - 1
-        L_loss = MgUtC * self.theta*L**(self.eps_l ** (-1)) / MPL -1
+        L_loss = self.theta*L**(self.eps_l ** (-1)) / MPL -1
         K_loss = Pk/MPK - 1
         I_loss =  Pk/Pkmodel -1 
         P_loss = Y/(C+I) - 1
